2d8b13b247b851b5226144efe30d6ccc2778b69b
Ankur Work <ankurwork@ankurs-mbp.lan>
2022-02-02 22:14:38 -0700

    add region

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
+  region        = "us-west-2"
   tags = {
     Name = "terraform-example"
   }
