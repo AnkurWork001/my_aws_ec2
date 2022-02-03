5998c7d911f73c2776ad7a56ebda8d7cbd75be86
Ankur Work <ankurwork@ankurs-mbp.lan>
2022-02-02 22:32:06 -0700

    add region

--- main.tf
+++ main.tf
@@ -7,7 +7,6 @@ provider "aws" {
 resource "aws_instance" "my_aws_wc2" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
-  region        = "us-west-2"
   tags = {
     Name = "my_aws_wc2"
   }
