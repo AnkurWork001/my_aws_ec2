de404536a3789b7e2f123e0c2c85d4176e975cef
Ankur Work <ankurwork@ankurs-mbp.lan>
2022-02-02 23:09:40 -0700

    adding access and secret key

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
 
-provider "aws" {
-  region = var.aws_region
-  access_key = var.access_key
-  secret_key = var.secret_key
-}
-
-
 resource "aws_instance" "my_aws_wc2" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
