e02d9927ae9142af3039e20b1243310212e05c41
Ankur Work <ankurwork@ankurs-mbp.lan>
2022-02-02 23:29:58 -0700

    adding access and secret key

--- variable.tf
+++ variable.tf
@@ -3,3 +3,14 @@ variable "aws_region" {
   default = "us-west-2"
 }
 
+variable "access_key" {
+  description = "aws access key"
+  type        = string
+  sensitive   = true
+}
+
+variable "secret_key" {
+  description = "aws access key"
+  type        = string
+  sensitive = true
+}
